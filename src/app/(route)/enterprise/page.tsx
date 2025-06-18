d29786fba41e1e4831ed744898ff355f49d69edd
sungjoon92 <wsjtjdwns92@naver.com>
2025-06-18 18:41:10 +0900

    feature/인증서 컴포넌트 구현완료

--- src/app/(route)/enterprise/page.tsx
+++ src/app/(route)/enterprise/page.tsx
@@ -5,6 +5,7 @@ import Mission from "@/components/enterprise/Mission";
 import Vision from "@/components/enterprise/Vision";
 import { historyData } from "@/data/historyData";
 import { awardsData } from "@/data/awardsData";
+import Certificates from "@/components/enterprise/Certificates";
 const EnterprisePage: React.FC = () => {
   return (
     <div className="w-[70%] m-auto flex flex-col ">
@@ -13,6 +14,7 @@ const EnterprisePage: React.FC = () => {
       <Vision />
       <HistoryList data={historyData} />
       <Awards data={awardsData} />
+      <Certificates />
     </div>
   );
 };

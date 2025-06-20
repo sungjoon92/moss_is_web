b0c8f799ce5e2f2416c98ad4bc3afdfa1b2daec8
sungjoon92 <wsjtjdwns92@naver.com>
2025-06-20 18:03:23 +0900

    feature/404NotFound 페이지 제작 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Source_Sans_3, Manrope } from "next/font/google";
-
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { siteDetails } from "@/data/siteDetails";

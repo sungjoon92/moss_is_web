66328b85e044f4e04873746453417a857a4273e6
sungjoon92 <wsjtjdwns92@naver.com>
2025-06-30 12:40:15 +0900

    fix/jsdom inport문 추가

--- src/utils/sanitizeHtmlServer.ts
+++ src/utils/sanitizeHtmlServer.ts
@@ -1,6 +1,8 @@
+import { JSDOM } from "jsdom";
 import createDOMPurify from "dompurify";
 
-const window = require("jsdom").window;
+const jsdom = new JSDOM(""); // 인스턴스 생성
+const window = jsdom.window; // window 객체 추출
 const DOMPurify = createDOMPurify(window);
 
 export function sanitizeHtmlServer(dirty: string): string {

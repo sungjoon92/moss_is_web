ed9abc79f0cc5ad56714910b8cf8080ee21fa783
sungjoon92 <wsjtjdwns92@naver.com>
2025-07-03 19:01:33 +0900

    fix/axios.ts 불필요한 console.log 제거

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,8 +6,6 @@ const baseURL =
     ? process.env.API_BASE_URL || "http://localhost:3000/api" // 서버 환경
     : "/api"; // 클라이언트 환경
 
-console.log(process.env.API_BASE_URL);
-
 const instance = axios.create({
   baseURL,
   withCredentials: true,
